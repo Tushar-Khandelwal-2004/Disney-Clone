afc77efa5646319a0b1016706a2f7e3367817567
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-01-09 18:43:47 +0530

    Made the navbar Responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,46 +1,65 @@
 import logo from "./../assets/images/logo.png"
-import { HiHome,
+import {
+    HiHome,
     HiMagnifyingGlass,
     HiStar,
     HiPlayCircle,
-    HiTv } from "react-icons/hi2";
-import { HiPlus,HiDotsVertical } from "react-icons/hi";
+    HiTv
+} from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import Headeritem from "./Headeritem";
-export const Header=()=>{
-    const menu=[
+import { useState } from "react";
+export const Header = () => {
+    const [toggle,setToggle]=useState(false);
+    const menu = [
         {
-            name:'HOME',
-            icon:HiHome
+            name: 'HOME',
+            icon: HiHome
         },
         {
-            name:'SEARCH',
-            icon:HiMagnifyingGlass
+            name: 'SEARCH',
+            icon: HiMagnifyingGlass
         },
         {
-            name:'WATCH LIST',
-            icon:HiPlus
+            name: 'WATCH LIST',
+            icon: HiPlus
         },
         {
-            name:'ORIGINALS',
-            icon:HiStar
+            name: 'ORIGINALS',
+            icon: HiStar
         },
         {
-            name:'MOVIES',
-            icon:HiPlayCircle
+            name: 'MOVIES',
+            icon: HiPlayCircle
         },
         {
-            name:'SERIES',
-            icon:HiTv
+            name: 'SERIES',
+            icon: HiTv
         }
     ]
     return <div className="flex items-center justify-between p-5">
         <div className="flex gap-8 items-center ">
-        <img src={logo} className="w-[80px] md:w-[115px] object-cover"/>
-        {menu.map((item)=>(
-            <Headeritem name={item.name} Icon={item.icon}/>
-        ))}
+            <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
+            <div className="hidden md:flex gap-8">
+                {menu.map((item) => (
+                    <Headeritem name={item.name} Icon={item.icon} />
+                ))}
+            </div>
+            <div className="flex md:hidden gap-5">
+                {menu.map((item, index) => index < 3 && (
+                    <Headeritem name={''} Icon={item.icon} />
+                ))}
+                <div className="md:hidden" onClick={()=>{setToggle(c=>!c)}}>
+                    <Headeritem name={''} Icon={HiDotsVertical} />
+                    {toggle?<div className="absolute mt-3 bg-[#121212] border-[1px] p-3 border-gray-700 px-5 py-4">
+                        {menu.map((item, index) => index > 2 && (
+                            <Headeritem name={item.name} Icon={item.icon} />
+                        ))}
+                    </div>:null}
+                </div>
+            </div>
         </div>
         <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-        className='w-[40px] rounded-full'/>
+            className='w-[40px] rounded-full' />
     </div>
 }
